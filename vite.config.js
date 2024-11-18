1dbc7f0e91f8b9f9cb31f93d173251ab7c711f6e
estif4 <estifk6@gmail.com>
2024-11-18 23:10:28 +0300

    new edit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    base: "/React_quiz"
-  },
+  base: "/React_quiz/", // Adjust this to your GitHub repository name
 });
